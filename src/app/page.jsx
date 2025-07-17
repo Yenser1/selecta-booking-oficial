3a97545f1cc8fdf8ffcc8e2828112c9395c1f5f0
Yenser1 <yenserdavidvasquezhernandez@gmail.com>
2025-07-16 23:57:56 -0400

    First COMMIT

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import { Monoton } from "next/font/google"
+
+const monoton = Monoton({
+    subset: ['latin'],
+    weight: '400'
+    
+})
+
+function LandingPage(){
+    return (
+        <section className={`w-full h-screen flex justify-center items-center`}
+        >
+            <h1 className={`${monoton.className} text-9xl`}
+            >This is SELECTA BOOKING</h1>
+        </section>
+        
+    )
+}
+
+export default LandingPage

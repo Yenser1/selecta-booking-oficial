9e983e05225d3d616dc3460b28a252f49a967454
Yenser1 <yenserdavidvasquezhernandez@gmail.com>
2025-10-24 00:53:48 -0400

    correctly done

--- src/app/AcercaDeNosotros/page.jsx
+++ src/app/AcercaDeNosotros/page.jsx
@@ -1,6 +1,5 @@
 import { Roboto_Mono } from "next/font/google";
 import Image from "next/image";
-import { useState } from "react";
 
 const roboto = Roboto_Mono({
     subsets: ["latin"],
--- src/app/Restaurantes/page.jsx
+++ src/app/Restaurantes/page.jsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import EmblaCarouselFade from "../../Components/EmblaFadeCarousel";
 import RESTAURANT from "../../consts/restuarants";
 import Box from "../../Components/ResBox";
-import ESTILOS from "../../consts/estilos";
-import ESTILOSinfo from "../../consts/estilosinfo";
 import { useEffect, useState } from "react";
 import { Roboto_Mono } from "next/font/google";
 
@@ -19,10 +17,10 @@ const robotoBold = Roboto_Mono({
 });
 
 function RestaurantesPage() {
-    const [style, setStyle] = useState("todas")//criollaDominicana,comidaRapida,parrillada,marisquerias,italiana,china,mexicana,arabe,japonesa,vegana
+    const [style, setStyle] = useState("todas"); //criollaDominicana,comidaRapida,parrillada,marisquerias,italiana,china,mexicana,arabe,japonesa,vegana
     const Restaurantes = [];
     const RestCard = [];
-    let foodStyles
+    let foodStyles;
 
     for (let i = 0; i < RESTAURANT.length; i++) {
         Restaurantes.push(
@@ -37,33 +35,33 @@ function RestaurantesPage() {
                 <h1 className="absolute h1 text-center w-full">{RESTAURANT[i].name}</h1>
             </div>
         );
-        
-            RestCard.push(
-                <Box nombre={RESTAURANT[i].name} estilo={RESTAURANT[i].estilo} imgURL={RESTAURANT[i].imgURL} descripcion={RESTAURANT[i].descripcion} link={RESTAURANT[i].link} key={RESTAURANT[i].name} />
-            );
-            console.log(RestCard)
-            if(style === "criollaDominicana"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 0)
-            }else if(style === "comidaRapida"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 1)
-            }else if(style === "parrillada"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 2)
-            }else if(style === "marisquerias"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 3)
-            }else if(style === "italiana"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 4)
-            }else if(style === "china"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 5)
-            }else if(style === "mexicana"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 6)
-            }else if(style === "arabe"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 7)
-            }else if(style === "japonesa"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 8)
-            }else if(style === "vegana"){
-                foodStyles = RestCard.filter(element => element.props.estilo === 9)
-            }
+
+        RestCard.push(
+            <Box nombre={RESTAURANT[i].name} estilo={RESTAURANT[i].estilo} imgURL={RESTAURANT[i].imgURL} descripcion={RESTAURANT[i].descripcion} link={RESTAURANT[i].link} key={RESTAURANT[i].name} />
+        );
+        console.log(RestCard);
+        if (style === "criollaDominicana") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 0);
+        } else if (style === "comidaRapida") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 1);
+        } else if (style === "parrillada") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 2);
+        } else if (style === "marisquerias") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 3);
+        } else if (style === "italiana") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 4);
+        } else if (style === "china") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 5);
+        } else if (style === "mexicana") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 6);
+        } else if (style === "arabe") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 7);
+        } else if (style === "japonesa") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 8);
+        } else if (style === "vegana") {
+            foodStyles = RestCard.filter((element) => element.props.estilo === 9);
         }
+    }
     const [isClient, setIsClient] = useState(false);
     useEffect(() => {
         setIsClient(true);
@@ -75,110 +73,141 @@ function RestaurantesPage() {
             <section>
                 <h1 className={`${robotoBold.className} h3 text-center text-amber-400 mb-5 underline`}>Filtra los estilos</h1>
                 <div className="flex flex-wrap gap-5 justify-center items-center">
-                    <button key={0} className={`${style === "criollaDominicana"? "selectedStyle":""} bg-yellow-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={0}
+                        className={`${style === "criollaDominicana" ? "selectedStyle" : ""} bg-yellow-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("criollaDominicana")
+                            setStyle("criollaDominicana");
                         }}
                         style={{
-                            boxShadow: `${style === "criollaDominicana"?"0 0 10px #CA8A04":""}`,
-                        }}>
+                            boxShadow: `${style === "criollaDominicana" ? "0 0 10px #CA8A04" : ""}`,
+                        }}
+                    >
                         <p>Criolla Dominicana</p>
                     </button>
-                    <button key={1} className={`${style === "comidaRapida"? "selectedStyle":""} bg-red-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={1}
+                        className={`${style === "comidaRapida" ? "selectedStyle" : ""} bg-red-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("comidaRapida")
+                            setStyle("comidaRapida");
                         }}
                         style={{
-                            boxShadow: `${style === "comidaRapida"?"0 0 10px #DC2626":""}`,
-                        }}>
+                            boxShadow: `${style === "comidaRapida" ? "0 0 10px #DC2626" : ""}`,
+                        }}
+                    >
                         <p>Comida Rapida</p>
                     </button>
-                    <button key={2} className={`${style === "parrillada"? "selectedStyle":""} bg-red-800 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={2}
+                        className={`${style === "parrillada" ? "selectedStyle" : ""} bg-red-800 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("parrillada")
+                            setStyle("parrillada");
                         }}
                         style={{
-                            boxShadow: `${style === "parrillada"?"0 0 10px #991B1B":""}`,
-                        }}>
+                            boxShadow: `${style === "parrillada" ? "0 0 10px #991B1B" : ""}`,
+                        }}
+                    >
                         <p>Parrillada</p>
                     </button>
-                    <button key={3} className={`${style === "marisquerias"? "selectedStyle":""} bg-sky-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={3}
+                        className={`${style === "marisquerias" ? "selectedStyle" : ""} bg-sky-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("marisquerias")
+                            setStyle("marisquerias");
                         }}
                         style={{
-                            boxShadow: `${style === "marisquerias"?"0 0 10px #0EA5E9":""}`,
-                        }}>
+                            boxShadow: `${style === "marisquerias" ? "0 0 10px #0EA5E9" : ""}`,
+                        }}
+                    >
                         <p>Marisquerias</p>
                     </button>
-                    <button key={4} className={`${style === "italiana"? "selectedStyle":""} bg-green-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={4}
+                        className={`${style === "italiana" ? "selectedStyle" : ""} bg-green-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("italiana")
+                            setStyle("italiana");
                         }}
                         style={{
-                            boxShadow: `${style === "italiana"?"0 0 10px #047857":""}`,
-                        }}>
+                            boxShadow: `${style === "italiana" ? "0 0 10px #047857" : ""}`,
+                        }}
+                    >
                         <p>Italiana</p>
                     </button>
-                    <button key={5} className={`${style === "china"? "selectedStyle":""} bg-red-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={5}
+                        className={`${style === "china" ? "selectedStyle" : ""} bg-red-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("china")
+                            setStyle("china");
                         }}
                         style={{
-                            boxShadow: `${style === "china"?"0 0 10px #B91C1C":""}`,
-                        }}>
+                            boxShadow: `${style === "china" ? "0 0 10px #B91C1C" : ""}`,
+                        }}
+                    >
                         <p>China</p>
                     </button>
-                    <button key={6} className={`${style === "mexicana"? "selectedStyle":""} bg-green-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={6}
+                        className={`${style === "mexicana" ? "selectedStyle" : ""} bg-green-600 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("mexicana")
+                            setStyle("mexicana");
                         }}
                         style={{
-                            boxShadow: `${style === "mexicana"?"0 0 10px #059669":""}`,
-                        }}>
+                            boxShadow: `${style === "mexicana" ? "0 0 10px #059669" : ""}`,
+                        }}
+                    >
                         <p>Mexicana</p>
                     </button>
-                    <button key={7} className={`${style === "arabe"? "selectedStyle":""} bg-lime-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={7}
+                        className={`${style === "arabe" ? "selectedStyle" : ""} bg-lime-700 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("arabe")
+                            setStyle("arabe");
                         }}
                         style={{
-                            boxShadow: `${style === "arabe"?"0 0 10px #65A30D":""}`,
-                        }}>
+                            boxShadow: `${style === "arabe" ? "0 0 10px #65A30D" : ""}`,
+                        }}
+                    >
                         <p>Arabe</p>
                     </button>
-                    <button key={8} className={`${style === "japonesa"? "selectedStyle":""} bg-red-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={8}
+                        className={`${style === "japonesa" ? "selectedStyle" : ""} bg-red-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("japonesa")
+                            setStyle("japonesa");
                         }}
                         style={{
-                            boxShadow: `${style === "japonesa"?"0 0 10px #EF4444":""}`,
-                        }}>
+                            boxShadow: `${style === "japonesa" ? "0 0 10px #EF4444" : ""}`,
+                        }}
+                    >
                         <p>Japonesa</p>
                     </button>
-                    <button key={9} className={`${style === "vegana"? "selectedStyle":""} bg-lime-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={9}
+                        className={`${style === "vegana" ? "selectedStyle" : ""} bg-lime-500 w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("vegana")
+                            setStyle("vegana");
                         }}
                         style={{
-                            boxShadow: `${style === "vegana"?"0 0 10px #84CC16":""}`,
+                            boxShadow: `${style === "vegana" ? "0 0 10px #84CC16" : ""}`,
                         }}
                     >
                         <p>Vegana</p>
                     </button>
-                    <button key={10} className={`${style === "todas"? "selectedStyle":""} bg-[#333] w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
+                    <button
+                        key={10}
+                        className={`${style === "todas" ? "selectedStyle" : ""} bg-[#333] w-max px-1.5 hover:scale-110 transition-all duration-150 rounded-md foodStyle`}
                         onClick={() => {
-                            setStyle("todas")
+                            setStyle("todas");
                         }}
                         style={{
-                            boxShadow: `${style === "todas"?"0 0 10px #333":""}`,
+                            boxShadow: `${style === "todas" ? "0 0 10px #333" : ""}`,
                         }}
                     >
                         <p>Todas</p>
                     </button>
                 </div>
             </section>
-            <section className="flex flex-wrap w-90% mt-20 justify-center">{style==="todas"?RestCard:foodStyles}</section>
+            <section className="flex flex-wrap w-90% mt-20 justify-center">{style === "todas" ? RestCard : foodStyles}</section>
         </main>
     );
 }
